2be26612909eb8943f7a435ff9c4058720d9cdf2
Robin Nabel <roblob@nurfuerspam.de>
2013-11-16 23:18:53 +0000

    misc

---
+++ DataBaseManipulation/DataExtraction.java
@@ -0,0 +1,13 @@
+package DataBaseManipulation;
+
+public class DataExtraction {
+
+	
+	//returns a json string of kez value pairs corresponding to
+	//all students within the class requested
+	public static String getListOfPhoneNumber(String class_id, String school_id) {
+		
+		return null;
+	}
+	
+}
